facee9d5afc2269d11ee3cc394d1c32daaea075d
BuildTools <unconfigured@null.spigotmc.org>
2021-10-15 20:00:50 -0500

    Final (as of right now) version

--- turing.py
+++ turing.py
@@ -7,7 +7,7 @@ def getTuples():
         userInput = input()
         if userInput == '.':
             running = False
-        elif len(userInput) == 5 and (userInput[3] == 'r' or userInput[3] == 'R' or userInput[3] == 'l' or userInput[3] == 'L'):
+        elif len(userInput) == 5 and (userInput[3] == 'r' or userInput[3] == 'R' or userInput[3] == 'l' or userInput[3] == 'L' or userInput[3] == 'N' or userInput[3] == 'n'):
             tuples.append(userInput)
         else:
             print("Invalid input. \n")
@@ -41,10 +41,12 @@ def createTupleDictionary(tuples):
             tupleDict["StartingTupleKey"] = tuples[i][0]
     for tuple in tuples:
         tupleDict[tuple[0]].append(tuple)
+    
 
     return tupleDict
 
 def readTape(tupleDict, tape, limit):
+    #Sets the starting tape index, as well as the starting key (the first one entered by the user)
     currentTapeIndex = 0
     startingTupleKey = None
     for x in tupleDict.keys():
@@ -54,38 +56,130 @@ def readTape(tupleDict, tape, limit):
     
     currentTupleKey = startingTupleKey
     for i in range(0, limit):
+        foundTuple = False
         for tuple in tupleDict[currentTupleKey]:
-            if tuple[1].__str__() == tape[currentTapeIndex]:
-                tape[currentTapeIndex] = tuple[2]
+
+            #Checks if the tuple's "read" character matches the one on the tape.
+            if tuple[1] == '*':
+               
+                foundTuple = True
+                if tuple[2] != '*':
+                    tape[currentTapeIndex] = tuple[2]
+                    
+                #If the tuple moves the pointer to the right, this will check if 
+                #the array is long enough to do that; if not, it will append 10 
+                #blank characters to the right of the tape.
                 if tuple[3] == 'R' or tuple[3] == 'r':
                     try:
                         currentTapeIndex += 1
                     except IndexError:
-                        for j in range(10):
-                            tape.append(' ')
-                        currentTapeIndex += 1
-                else:
-                    try:
-                        currentTapeIndex -= 1
-                    except IndexError:
-                        newTape = []
-                        for j in range(0, 9):
-                            newTape[j] = ' '
-                        for j in range(10, len(tape)+9):
-                            newTape[j] = tape[j - 10]
+                        tape.append(' ')
+                        #currentTapeIndex += 1
+                #Since Python takes negative indexes and uses them to show the
+                #last item in a list (instead of throwing an error), if the
+                #pointer goes past the left of the array, 10 blank characters will
+                #be added to the left.
+                elif tuple[3] == 'L' or tuple[3] == 'l':
+                    currentTapeIndex -= 1
+                    if currentTapeIndex < 0:
+                        newTape = [' ']
+                        for character in tape:
+                            newTape.append(character)
                         tape = newTape
                         currentTapeIndex = currentTapeIndex + 10
-                        currentTapeIndex -= 1
+                else:
+                    pass
+                #Prints tape in readable format.
                 tapeString = ' '.join(map(str, tape))
-                #print(*tape)
-                print(tapeString+"{"+currentTupleKey+"}")
-                currentTupleKey = tuple[4]
-                if i == limit:
+                print("{"+currentTupleKey+"}"+tapeString)
+                #The pointerTape draws an arrow to where the current tape index is
+                #at the end of an instruction.
+                pointerTape = tape.copy()
+                for j in range(0, len(pointerTape)):
+                    if j == currentTapeIndex:
+                        pointerTape[j] = '^'
+                    else:
+                        pointerTape[j] = ' '
+                print('   '+' '.join(map(str, pointerTape)))
+                noKeyFound = True
+                finalState = currentTupleKey
+                #Checks to see if there are any valid instructions left; if not, the
+                #machine will halt. 
+                for k in tupleDict.keys():
+                    if k == tuple[4]:
+                        currentTupleKey = tuple[4]
+                        noKeyFound = False
+                if noKeyFound:
+                    print("Halted!")
+                    print("Final state: "+finalState)
+                    return
+                #Checks if max iterations have been reached.
+                if i == limit - 1:
                     print("Ended: max iterations reached")
-            else: 
-                print("Halted!")
-                return
-    
+                    print("Final state: "+finalState)
+                break
+            elif tuple[1] == tape[currentTapeIndex]:
+               
+                foundTuple = True
+                if tuple[2] != '*':
+                    tape[currentTapeIndex] = tuple[2]
+                  
+                #If the tuple moves the pointer to the right, this will check if 
+                #the array is long enough to do that; if not, it will append 10 
+                #blank characters to the right of the tape.
+                if tuple[3] == 'R' or tuple[3] == 'r':
+                    try:
+                        currentTapeIndex += 1
+                    except IndexError:
+                        tape.append(' ')
+                        #currentTapeIndex += 1
+                #Since Python takes negative indexes and uses them to show the
+                #last item in a list (instead of throwing an error), if the
+                #pointer goes past the left of the array, 10 blank characters will
+                #be added to the left.
+                elif tuple[3] == 'L' or tuple[3] == 'l':
+                    currentTapeIndex -= 1
+                    if currentTapeIndex < 0:
+                        newTape = [' ']
+                        for character in tape:
+                            newTape.append(character)
+                        tape = newTape
+                        currentTapeIndex = currentTapeIndex + 10
+                else:
+                    pass
+                #Prints tape in readable format.
+                tapeString = ' '.join(map(str, tape))
+                print("{"+currentTupleKey+"}"+tapeString+"{"+tuple[4]+"}")
+                #The pointerTape draws an arrow to where the current tape index is
+                #at the end of an instruction.
+                pointerTape = tape.copy()
+                for j in range(0, len(pointerTape)):
+                    if j == currentTapeIndex:
+                        pointerTape[j] = '^'
+                    else:
+                        pointerTape[j] = ' '
+                print('   '+' '.join(map(str, pointerTape)))
+                noKeyFound = True
+                finalState = currentTupleKey
+                #Checks to see if there are any valid instructions left; if not, the
+                #machine will halt. 
+                for k in tupleDict.keys():
+                    if k == tuple[4]:
+                        currentTupleKey = tuple[4]
+                        noKeyFound = False
+                if noKeyFound:
+                    print("Halted!")
+                    print("Final state: "+finalState)
+                    return
+                #Checks if max iterations have been reached.
+                if i == limit - 1:
+                    print("Ended: max iterations reached")
+                    print("Final state: "+finalState)
+                break
+
+        if foundTuple == False:
+            print("Halted: no valid instruction to continue.")
+
 def main():
     tupleDict = createTupleDictionary(getTuples())
     print(tupleDict)
